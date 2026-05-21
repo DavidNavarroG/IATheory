4050e152b85417547299a6c8fa933f5be350af67
david.navarro.girones@gmail.com <dnavarro@tds508.pic.es>
2026-05-21 15:43:07 +0200

    Removed the likelihood directory.

--- src/IATheory/__init__.py
+++ src/IATheory/__init__.py
@@ -1,5 +1,4 @@
 from . import compute_observables
 from . import read_data
-from . import likelihood
 
 __version__ = "0.0.1"
--- src/IATheory/likelihood/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import read_config, run_sampler
-
-__all__ = ["read_config", "run_sampler"]
-
-__version__ = "0.0.1"
